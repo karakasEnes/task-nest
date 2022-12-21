07862dd3bbca28260f3ee054bb95ea9e07647ce2
Enes Karakas <enes.coding@gmail.com>
2022-12-21 20:39:57 +0300

    postgres for production

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -13,7 +13,7 @@ import { join } from 'path';
       rootPath: join(__dirname, '..', 'public'),
     }),
     ConfigModule.forRoot({
-      envFilePath: ['.env'],
+      envFilePath: ['.env', '.env.prod'],
       validationSchema: configValidationSchema,
     }),
     TasksModule,
@@ -24,12 +24,28 @@ import { join } from 'path';
       useFactory: async (configService: ConfigService) => {
         const isProduction = configService.get('STAGE') === 'prod';
 
+        console.log('isProduction', isProduction);
+
+        const HOST = isProduction
+          ? configService.get('POSTGRES_HOST_PROD')
+          : 'localhost';
+        const PSW = isProduction
+          ? configService.get('POSTGRES_PASSWORD_PROD')
+          : configService.get('POSTGRES_PASSWORD');
+        const USERNAME = isProduction
+          ? configService.get('POSTGRES_USERNAME_PROD')
+          : configService.get('POSTGRES_USERNAME');
+
         return {
+          ssl: isProduction,
+          extra: {
+            ssl: isProduction ? { rejectUnauthorized: false } : null,
+          },
           type: 'postgres',
-          host: 'localhost',
+          host: HOST,
           port: parseInt(configService.get('POSTGRES_PORT')),
-          username: configService.get('POSTGRES_USERNAME'),
-          password: configService.get('POSTGRES_PASSWORD'),
+          username: USERNAME,
+          password: PSW,
           database: configService.get('POSTGRES_DB'),
           autoLoadEntities: true,
           synchronize: true,
